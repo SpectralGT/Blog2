b33eea5a4c8df4f7e76f1494c1aab2e8d06214e6
Atharv Singh <atharvsingh1396@gmail.com>
2024-09-28 12:20:45 +0530

    added posts and changed styles

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default function PostCard({post}) {
+export default function PostCard({ post }) {
   return (
-    <div className='post-card upper-text'>
-      
-        {/* <img className='h-40 w-64' src={post.frontMatter.thumbnail} alt="postCardImage" /> */}
-        <div className=''>
-            <h2 className='title pink-glow-100'>{post.frontMatter.title}</h2>
-            <p className='description white-glow-100'>{post.frontMatter.description}</p>
-        </div>
-        <div className='date-time white-glow-100'>
-            <h2 className='date'>{post.frontMatter.date}</h2>
-            <p className='time'>{post.frontMatter.readTime} min read</p>
-        </div>
-
+    <div className="post-card upper-text">
+      {/* <img className='h-40 w-64' src={post.frontMatter.thumbnail} alt="postCardImage" /> */}
+      <div className="">
+        <h2 className="title pink-glow-100">{post.frontMatter.title}</h2>
+        <p className="description white-glow">
+          {post.frontMatter.description}
+        </p>
+        <div className="spacer"></div>
+      </div>
+      <div className="date-time white-glow-100">
+        <h2 className="date">{post.frontMatter.date}</h2>
+        <p className="time">{post.frontMatter.readTime} min read</p>
+      </div>
     </div>
-  )
+  );
 }
