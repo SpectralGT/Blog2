027e7bbd9776ec302a60ac5784263e047206ff02
SpectralGT <atharvsingh1396@gmail.com>
2025-05-04 14:47:08 +0530

    updated post listing to sort from new to old

--- pages/index.js
+++ pages/index.js
@@ -9,90 +9,98 @@ import LoadingScreen from "../components/LoadingScreen";
 import { useEffect, useState } from "react";
 
 export default function Home(props) {
-  // Track loading percentage
-  const [loading, setLoading] = useState(0);
-  // Track if loading screen should be shown
-  const [showLoading, setShowLoading] = useState(true);
+	// Track loading percentage
+	const [loading, setLoading] = useState(0);
+	// Track if loading screen should be shown
+	const [showLoading, setShowLoading] = useState(true);
 
-  useEffect(() => {
-    // Check if the loading screen has already been shown
-    const hasShownLoading = sessionStorage.getItem("showLoading");
+	useEffect(() => {
+		// Check if the loading screen has already been shown
+		const hasShownLoading = sessionStorage.getItem("showLoading");
 
-    if (hasShownLoading) {
-      // Skip loading screen if it has been shown before
-      setShowLoading(false);
-      return;
-    }
+		if (hasShownLoading) {
+			// Skip loading screen if it has been shown before
+			setShowLoading(false);
+			return;
+		}
 
-    // Display the loading screen and update the percentage
-    const id = setInterval(() => {
-      setLoading((loading) => loading + 1);
-    }, 20);
+		// Display the loading screen and update the percentage
+		const id = setInterval(() => {
+			setLoading((loading) => loading + 1);
+		}, 20);
 
-    // Stop the loading animation at 100%
-    if (loading >= 100) {
-      clearInterval(id);
-      sessionStorage.setItem("showLoading", "true");
-      setShowLoading(false);
-    }
+		// Stop the loading animation at 100%
+		if (loading >= 100) {
+			clearInterval(id);
+			sessionStorage.setItem("showLoading", "true");
+			setShowLoading(false);
+		}
 
-    return () => clearInterval(id);
-  }, [loading]);
+		return () => clearInterval(id);
+	}, [loading]);
 
-  let body = <div></div>;
+	let body = <div></div>;
 
-  // Conditional rendering based on showLoading state
-  if (showLoading && loading <= 100) {
-    body = <LoadingScreen percentage={loading} />;
-  } else {
-    body = (
-      <div>
-        <Hero />
-        {props.posts.length > 0 ? (
-          <div className="listing">
-            {props.posts.map((post, index) => (
-              <Link href={`/posts/${post.slug}`} key={index}>
-                <Postcard post={post} />
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <h2 className="font-sans text-3xl">No posts yet</h2>
-        )}
-      </div>
-    );
-  }
+	// Conditional rendering based on showLoading state
+	if (showLoading && loading <= 100) {
+		body = <LoadingScreen percentage={loading} />;
+	} else {
+		body = (
+			<div>
+				<Hero />
+				{props.posts.length > 0 ? (
+					<div className="listing">
+						{[...props.posts]
+							.sort((a, b) => new Date(b.frontMatter.date) - new Date(a.frontMatter.date))
+							.map((post, index) => (
+								<Link
+									href={`/posts/${post.slug}`}
+									key={index}>
+									<Postcard post={post} />
+								</Link>
+							))}
+					</div>
+				) : (
+					<h2 className="font-sans text-3xl">No posts yet</h2>
+				)}
+			</div>
+		);
+	}
 
-  return (
-    <div className="">
-      <Head>
-        <link rel="icon" href="/favicon.svg" sizes="any" />
-        <title>Atharv&apos;s Blog</title>
-        <meta
-          name="google-site-verification"
-          content="kRbwjumXat52-e3mUB7tt-faI1jx9mY0x1EMOPC5egE"
-        />
-      </Head>
-      {body}
-    </div>
-  );
+	return (
+		<div className="">
+			<Head>
+				<link
+					rel="icon"
+					href="/favicon.svg"
+					sizes="any"
+				/>
+				<title>Atharv&apos;s Blog</title>
+				<meta
+					name="google-site-verification"
+					content="kRbwjumXat52-e3mUB7tt-faI1jx9mY0x1EMOPC5egE"
+				/>
+			</Head>
+			{body}
+		</div>
+	);
 }
 
 // Fetching the posts.
 export async function getStaticProps() {
-  let files = fs.readdirSync(path.join("posts"));
-  files = files.filter((file) => file.split(".")[1] === "mdx");
-  const posts = files.map((file) => {
-    const fileData = fs.readFileSync(path.join("posts", file), "utf-8");
-    const { data } = matter(fileData);
-    return {
-      frontMatter: data,
-      slug: file.split(".")[0],
-    };
-  });
-  return {
-    props: {
-      posts,
-    },
-  };
+	let files = fs.readdirSync(path.join("posts"));
+	files = files.filter((file) => file.split(".")[1] === "mdx");
+	const posts = files.map((file) => {
+		const fileData = fs.readFileSync(path.join("posts", file), "utf-8");
+		const { data } = matter(fileData);
+		return {
+			frontMatter: data,
+			slug: file.split(".")[0],
+		};
+	});
+	return {
+		props: {
+			posts,
+		},
+	};
 }
